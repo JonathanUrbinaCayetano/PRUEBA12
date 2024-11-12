c62fe4c83df64a0fdf14e479f4b725929600d864
jonathan <jhon7122298039@gmail.com>
2024-11-11 23:09:00 -0600

    first commit

---
+++ pri/src/Trabajadores/Docentes.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from 'react';
+import '../App.css';
+
+const ListaDocentes = () => {
+    const [docentes, setDocentes] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('http://localhost/apiprueba/api.php');
+                const data = await response.json();
+                setDocentes(data);
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            }
+        };
+
+        fetchData(); // Fetch data initially
+
+        // Set interval to fetch data every 5 seconds
+        const intervalId = setInterval(fetchData, 5000);
+
+        // Cleanup interval on component unmount
+        return () => clearInterval(intervalId);
+    }, []);
+
+    return (
+        <div>
+            <h1 className="App App-link">Docentes Ingeniería Informática TESSFP</h1>
+            <div>
+                {docentes.map((docente) => (
+                    <div key={docente.issemyn}>
+                        <div>
+                            clave ISSEMYN: <strong>{docente.issemyn}</strong>
+                        </div>
+                        <div>
+                            <p>Nombre: <strong>{docente.nombre}</strong></p>
+                        </div>
+                        <hr />
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default ListaDocentes;
